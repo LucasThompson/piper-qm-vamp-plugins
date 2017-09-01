95a81e82b15af681658eecaf1d7d6186426a147a
Lucas Thompson <dev@lucas.im>
2017-09-01 12:34:55 +0100

    Remove build artifacts

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-import { WebWorkerService } from 'piper-js/web-worker';
-export declare const createService: () => WebWorkerService;
-export default createService;
--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const web_worker_1 = require("piper-js/web-worker");
-const QmWorker = require("worker-loader?inline&fallback=false!./worker-server.js");
-exports.createService = () => new web_worker_1.WebWorkerService(new QmWorker(), web_worker_1.countingIdProvider(0));
-exports.default = exports.createService;
